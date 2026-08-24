847089d73f3ae3bc50cfe8a53b00f419dc458bb9
OmarMohammed2011 <omar.tanzeel4133@gmail.com>
2026-08-24 11:09:11 -0500

    config.ts

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: "127.0.0.1",
-      port: 1111,
+      host: "muskellunge.aternos.host",
+      port: 35644,
     },
     tls: undefined,
   },
